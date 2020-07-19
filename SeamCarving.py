1e4997aa6f4901ecd53d1d5fc5cba097c0097c5f
Frank Ge <fge@princeton.edu>
2020-07-19 12:04:32 -0400

    Organization

--- SeamCarving.py
+++ SeamCarving.py
@@ -3,7 +3,12 @@ import cv2
 from skimage import filters
 import datetime
 
+# Picture class to process images using cv2
+# Contains the methods to calculate and remove seams from images
 class Picture:
+
+    # initialize with dimensions and energy matrix
+    # the actual image in array form stored in pic.im
     def __init__(self, filename):
         self.im = cv2.imread(filename)
         self.width = len(self.im[0])
@@ -11,6 +16,8 @@ class Picture:
         grayscale = cv2.cvtColor(self.im, cv2.COLOR_BGR2GRAY)
         self.energy = filters.sobel(grayscale) 
 
+    # dynamic programming approach to calculating minimum energy path
+    # note: another implementation can use Dijkstra's alg at the cost of overhead
     def find_seams(self):
         distTo = self.energy.copy()
         edgeTo = np.zeros((self.height, self.width))
@@ -29,6 +36,7 @@ class Picture:
 
         return distTo, edgeTo.astype(int)
 
+    # helper function to alter the image array and return a boolean mask
     def create_mask(self):
         distTo, edgeTo = self.find_seams()
         mask = np.ones((self.height, self.width), dtype = np.bool)
@@ -44,6 +52,7 @@ class Picture:
 
         return mask
 
+    # remove seam of width 3 pixels
     def remove_seam(self, mask):
         mask = np.stack([mask]*3, axis=2)
         self.im = self.im[mask].reshape(self.height, self.width - 3, 3)
@@ -51,6 +60,7 @@ class Picture:
         grayscale = cv2.cvtColor(self.im, cv2.COLOR_BGR2GRAY)
         self.energy = filters.sobel(grayscale) 
 
+    # highlight seam red before deletion for visualization
     def highlight(self, mask):
         self.im[mask == False] = [0,0,255]
 
@@ -58,19 +68,8 @@ class Picture:
         
 if __name__ == "__main__":
     pic = Picture('./HJoceanSmall.png')
-    pic.highlight()
-    cv2.waitKey(0)
-    '''
-    print(pic.im.size)
-    iter = len(pic.im[0])//3
-    for i in range(iter):
-        now = datetime.datetime.now()
-        pic.remove_seam()
-        then = datetime.datetime.now()
-        print(then - now)
-
-    print(pic.im.size)
+    pic.highlight(pic.create_mask())
     cv2.imshow('test', pic.im)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    '''
+   
